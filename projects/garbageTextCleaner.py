56ba2ff229cd156c3312fe9541e3a2ebec37257c
Prakhar Gandhi <gprakhar0@gmail.com>
2024-01-13 09:06:32 +0530

    Update garbageTextCleaner.py

--- projects/garbageTextCleaner.py
+++ projects/garbageTextCleaner.py
@@ -6,9 +6,12 @@ def garbageCleaner(garbage_text):
 	return dict
 
 def garbagesCleaner(garbage_texts):
-	rv = [garbageCleaner(garbage_text) for garbage_text in garbage_texts]
-	rv = list(set(rv))
-	return rv
+    rv = [garbageCleaner(garbage_text) for garbage_text in garbage_texts]
+    dv = {}
+    for garbage_text in garbage_texts:
+    	dv[garbage_text] = ""
+    rv = list(dv.keys())
+    return rv
 
 if __name__=="__main__":
 	garbage_text = 'âœ” No problems'
